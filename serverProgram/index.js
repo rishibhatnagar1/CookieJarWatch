79e5acdc270e7b43f3b7531a861299b97c125e1e
Rishi Bhatnagar <rishibhatnagar1@gmail.com>
2015-01-20 20:03:47 +0530

    Update index.js

--- serverProgram/index.js
+++ serverProgram/index.js
@@ -38,7 +38,7 @@ res.send("none");
 
 app.use(bodyParser.json());
 //app.use(connect.bodyParser({strict: false}));
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
 
   var host = server.address().address
   var port = server.address().port
